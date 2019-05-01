6b4ba429194e460945709911653c93ff3a10b010
Morris Hafner <hafnermorris@gmail.com>
2019-05-01 01:58:22 +0100

    yapf reformat

--- conanfile.py
+++ conanfile.py
@@ -8,13 +8,17 @@ class KhronosOpenCLICDLoaderConan(ConanFile):
     name = "khronos-opencl-icd-loader"
     version = "20190412"
     description = "The OpenCL ICD Loader"
-    topics = ("conan", "opencl", "opencl-icd-loader", "build-system", "icd-loader")
+    topics = ("conan", "opencl", "opencl-icd-loader", "build-system",
+              "icd-loader")
     url = "https://github.com/bincrafters/conan-khronos-opencl-icd-loader"
     homepage = "https://github.com/KhronosGroup/OpenCL-ICD-Loader"
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "Apache-2.0"
     exports = ["LICENSE.md"]
-    exports_sources = ["CMakeLists.txt", "0001-static-library.patch", "0002-Work-around-missing-declarations-in-MinGW-headers.patch"]
+    exports_sources = [
+        "CMakeLists.txt", "0001-static-library.patch",
+        "0002-Work-around-missing-declarations-in-MinGW-headers.patch"
+    ]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {"fPIC": [True, False], "shared": [True, False]}
@@ -33,7 +37,8 @@ class KhronosOpenCLICDLoaderConan(ConanFile):
     def source(self):
         commit = "66ecca5dce2c4425a48bdb0cf0de606e4da43ab5"
         sha256 = "3af9efaf9ebc68e1fb18b7904ec71004a9e71cf074e2ce70b719382b65f2b609"
-        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, commit), sha256=sha256)
+        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, commit),
+                  sha256=sha256)
         extracted_dir = "OpenCL-ICD-Loader-" + commit
         os.rename(extracted_dir, self._source_subfolder)
 
@@ -43,14 +48,20 @@ class KhronosOpenCLICDLoaderConan(ConanFile):
         return cmake
 
     def build(self):
-        tools.patch(base_path=self._source_subfolder, patch_file="0001-static-library.patch")
+        tools.patch(base_path=self._source_subfolder,
+                    patch_file="0001-static-library.patch")
         if self.settings.get_safe("os.subsystem") in ["msys", "msys2"]:
-            tools.patch(base_path=self._source_subfolder, patch_file="0002-Work-around-missing-declarations-in-MinGW-headers.patch")
+            tools.patch(
+                base_path=self._source_subfolder,
+                patch_file=
+                "0002-Work-around-missing-declarations-in-MinGW-headers.patch")
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="LICENSE",
+                  dst="licenses",
+                  src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
 
